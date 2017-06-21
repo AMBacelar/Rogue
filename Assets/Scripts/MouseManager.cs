e3b72510455a159b2b27d58eec35f4d6349bd4d6
Adilson Bacelar <a.m.bacelar@gmail.com>
2017-06-21 21:30:42 +0100

    player moves along path

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -6,17 +6,28 @@ public class MouseManager : MonoBehaviour
     public MapHandler CurrentMap;
     public GameObject MapHolder;
 
+    GameObject selectedUnit;
+
     // Use this for initialization
     void Start()
     {
         GameObject MapHolder = GameObject.Find("Map");
         CurrentMap = MapHolder.GetComponent<MapHandler>();
+
+        selectedUnit = GameObject.FindGameObjectWithTag("Player");
     }
     
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("1");
+            selectedUnit.GetComponent<Unit>().MoveNextTile();
+        }
+
         if (Input.GetMouseButtonDown(0))
         {            
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);

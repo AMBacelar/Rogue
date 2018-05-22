ec16b1e8ea88dc31ecc072fc589c6f047c49e11d
ambacelar <a.m.bacelar@gmail.com>
2018-05-22 17:26:56 +0100

    update Unity to 2018.1.1f1, removed initial implementation of pathfinding, imported RoyT.Astar library and replaced usages, massive difference

--- Assets/Actors/EnemyActor.cs
+++ Assets/Actors/EnemyActor.cs
@@ -1,27 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using RoyT.AStar;
 
 public class EnemyActor : Actor
 {
-	GameObject target;
 	BoardPosition targetPos;
 	BoardPosition position;
 
-	List<Node> solution = new List<Node>();
 	void Awake()
 	{
-		target = GameObject.FindGameObjectWithTag("Player");
-		targetPos = target.GetComponent<BoardPosition>();
+		targetPos = GameObject.FindGameObjectWithTag("Player").GetComponent<BoardPosition>();
 		position = gameObject.GetComponentInParent<BoardPosition>();
 	}
 	public override Action GetAction()
 	{
-		solution = Astar.GeneratePath(position, targetPos, BoardManager.instance.graph);
+		Position[] solution = BoardManager.instance.grid.GetPath(new Position(position.X, position.Y), new Position(targetPos.X, targetPos.Y), MovementPatterns.Hexagonal, 50);
 
-		if (solution.Count > 1)
+		if (solution.Length > 1)
 		{
-			Node node = solution[1];
-			IntVector2 direction = IntVector2.GetDirection(node.x - gameObject.GetComponentInParent<BoardPosition>().X, node.y - gameObject.GetComponentInParent<BoardPosition>().Y);
+			Position node = solution[1];
+			IntVector2 direction = IntVector2.GetDirection(node.X - gameObject.GetComponentInParent<BoardPosition>().X, node.Y - gameObject.GetComponentInParent<BoardPosition>().Y);
 			return GetAttackAction(direction);
 		}
 		return GetComponent<RestAction>();
--- Assets/Game/BoardManager.cs
+++ Assets/Game/BoardManager.cs
@@ -1,170 +1,179 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RoyT.AStar;
 
 public class BoardManager : MonoBehaviour
 {
 
-    public static BoardManager instance = null;
-    public int width;
-    public int height;
-    public int depth = 1;
-    public int monsterDensity = 30;
+	public static BoardManager instance = null;
+	public int width;
+	public int height;
+	public int depth = 1;
+	public int monsterDensity = 30;
 
-    public float xOffset = 0.939f;
-    public float zOffset = 0.814f;
+	public float xOffset = 0.939f;
+	public float zOffset = 0.814f;
 
-    public Hex[,] HexGrid;
-    public Node[,] graph;
-    private Dictionary<IntVector2, BoardPosition> dynamicPositions = new Dictionary<IntVector2, BoardPosition>();
+	public Hex[,] HexGrid;
+	public RoyT.AStar.Grid grid;
+	private Dictionary<IntVector2, BoardPosition> dynamicPositions = new Dictionary<IntVector2, BoardPosition>();
 
-    public TileType[] tileTypes;
+	public TileType[] tileTypes;
 
-    MapGenerator generator = new MapGenerator();
-    FeatureGenerator featureGenerator = new FeatureGenerator();
+	MapGenerator generator = new MapGenerator();
+	FeatureGenerator featureGenerator = new FeatureGenerator();
 
-    public void Initialise()
-    {
-        if (instance == null) instance = this;
-        else Debug.LogError("More than one BoardManager");
-    }
+	public void Initialise()
+	{
+		if (instance == null) instance = this;
+		else Debug.LogError("More than one BoardManager");
+	}
 
-    public void Awake()
-    {
-        Initialise();
-        HexGrid = generator.GenerateMap(width, height);
-        graph = Astar.GenerateGraph(HexGrid);
-        LoadMap(HexGrid);
-        Populate();
-    }
+	public void Awake()
+	{
+		Initialise();
+		HexGrid = generator.GenerateMap(width, height);
+		InitAStar(HexGrid);
+		LoadMap(HexGrid);
+		Populate();
+	}
 
-    public void Populate()
-    {
-        foreach (var hex in HexGrid)
-        {
-            if (IsOccupied(new IntVector2(hex.Q, hex.R)) == false && IsPassable(hex.Q, hex.R))
-                featureGenerator.Populate(new IntVector2(hex.Q, hex.R), monsterDensity, depth);
-        }
-    }
+	public void InitAStar(Hex[,] mapIn)
+	{
+		grid = new RoyT.AStar.Grid(mapIn.GetLength(0), mapIn.GetLength(1), 1.0f);
+		foreach (Hex hex in mapIn)
+		{
+			if (hex.isWalkable)
+			{
+				grid.SetCellCost(new Position(hex.Q, hex.R), tileTypes[hex.tileType].movementCost);
+			}
+			else
+			{
+				grid.BlockCell(new Position(hex.Q, hex.R));
+			}
+		}
+	}
 
-    public void UnregisterDynamicBoardPosition(BoardPosition toUnregister)
-    {
-        if (!dynamicPositions.ContainsKey(toUnregister.Position) || dynamicPositions[toUnregister.Position] != toUnregister)
-        {
-            Debug.LogError(toUnregister.name + "is not registered at it's board position.");
-            return;
-        }
-        dynamicPositions[toUnregister.Position] = null;
-    }
+	public void Populate()
+	{
+		foreach (var hex in HexGrid)
+		{
+			if (IsOccupied(new IntVector2(hex.Q, hex.R)) == false && IsPassable(hex.Q, hex.R))
+				featureGenerator.Populate(new IntVector2(hex.Q, hex.R), monsterDensity, depth);
+		}
+	}
 
-    public void RegisterDynamicBoardPosition(BoardPosition toRegister)
-    {
-        if (MapUtilityMethods.IsOutOfBounds(HexGrid, toRegister.X, toRegister.Y))
-        {
-            Debug.LogError("Attempt to RegisterActor out of board boaunds");
-            return;
-        }
+	public void UnregisterDynamicBoardPosition(BoardPosition toUnregister)
+	{
+		if (!dynamicPositions.ContainsKey(toUnregister.Position) || dynamicPositions[toUnregister.Position] != toUnregister)
+		{
+			Debug.LogError(toUnregister.name + "is not registered at it's board position.");
+			return;
+		}
+		dynamicPositions[toUnregister.Position] = null;
+	}
 
-        if (IsOccupied(toRegister.Position))
-        {
-            Debug.LogError("Attempt to RegisterActor occupied board position " + GetOccupied(toRegister.Position).name);
-            return;
-        }
-        dynamicPositions[toRegister.Position] = toRegister;
-    }
+	public void RegisterDynamicBoardPosition(BoardPosition toRegister)
+	{
+		if (MapUtilityMethods.IsOutOfBounds(HexGrid, toRegister.X, toRegister.Y))
+		{
+			Debug.LogError("Attempt to RegisterActor out of board boaunds");
+			return;
+		}
 
-    public bool IsOccupied(Hex position){
-        return IsOccupied(new IntVector2(position.Q,position.R));
-    }
+		if (IsOccupied(toRegister.Position))
+		{
+			Debug.LogError("Attempt to RegisterActor occupied board position " + GetOccupied(toRegister.Position).name);
+			return;
+		}
+		dynamicPositions[toRegister.Position] = toRegister;
+	}
 
-    public bool IsOccupied(Node node){
-        return IsOccupied(new IntVector2(node.x,node.y));
-    }
+	public bool IsOccupied(Hex position)
+	{
+		return IsOccupied(new IntVector2(position.Q, position.R));
+	}
 
-    public bool IsOccupied(IntVector2 position)
-    {
-        return GetOccupied(position) != null;
-    }
+	public bool IsOccupied(IntVector2 position)
+	{
+		return GetOccupied(position) != null;
+	}
 
-    public BoardPosition GetOccupied(IntVector2 position)
-    {
-        if (!dynamicPositions.ContainsKey(position))
-        {
-            return null;
-        }
-        return dynamicPositions[position];
-    }
+	public BoardPosition GetOccupied(IntVector2 position)
+	{
+		if (!dynamicPositions.ContainsKey(position))
+		{
+			return null;
+		}
+		return dynamicPositions[position];
+	}
 
-    public bool IsWithinBounds(int x, int y)
-    {
-        return !MapUtilityMethods.IsOutOfBounds(HexGrid, x, y);
-    }
+	public bool IsWithinBounds(int x, int y)
+	{
+		return !MapUtilityMethods.IsOutOfBounds(HexGrid, x, y);
+	}
 
-    public bool IsPassable(int x, int y)
-    {
-        return HexGrid[x, y].isWalkable && IsWithinBounds(x, y) && HexGrid[x, y].tileType == 0;
-    }
+	public bool IsPassable(int x, int y)
+	{
+		return HexGrid[x, y].isWalkable && IsWithinBounds(x, y) && HexGrid[x, y].tileType == 0;
+	}
 
-    public bool IsPassable(IntVector2 position)
-    {
-        return IsPassable(position.X, position.Y);
-    }
+	public bool IsPassable(IntVector2 position)
+	{
+		return IsPassable(position.X, position.Y);
+	}
 
-    public Hex GetHex(int x, int y)
-    {
-        return HexGrid[x, y];
-    }
+	public Hex GetHex(int x, int y)
+	{
+		return HexGrid[x, y];
+	}
 
-    public Hex GetHex(IntVector2 pos)
-    {
-        return GetHex(pos.X, pos.Y);
-    }
+	public Hex GetHex(IntVector2 pos)
+	{
+		return GetHex(pos.X, pos.Y);
+	}
 
-    public Vector3 TileCoordToWorldCoord(int x, int y)
-    {
-        return new Vector3(xOffset * (x + y / 2f), 0, y * zOffset);
-    }
+	public Vector3 TileCoordToWorldCoord(int x, int y)
+	{
+		return new Vector3(xOffset * (x + y / 2f), 0, y * zOffset);
+	}
 
-    public int Heuristics(Node a, Node b)
-    {
-        return Heuristics(new Hex(a.x, a.y), new Hex(b.x, b.y));
-    }
+	public int Heuristics(IntVector2 a, IntVector2 b)
+	{
+		return Heuristics(new Hex(a.X, a.Y), new Hex(b.X, b.Y));
+	}
 
-    public int Heuristics(IntVector2 a, IntVector2 b)
-    {
-        return Heuristics(new Hex(a.X, a.Y), new Hex(b.X, b.Y));
-    }
+	public int Heuristics(BoardPosition a, BoardPosition b)
+	{
+		return Heuristics(new Hex(a.X, a.Y), new Hex(b.X, b.Y));
+	}
 
-    public int Heuristics(BoardPosition a, BoardPosition b)
-    {
-        return Heuristics(new Hex(a.X, a.Y), new Hex(b.X, b.Y));
-    }
+	public int Heuristics(Hex a, Hex b)
+	{
+		return MapUtilityMethods.Heuristics(a, b);
+	}
 
-    public int Heuristics(Hex a, Hex b)
-    {
-        return MapUtilityMethods.Heuristics(a, b);
-    }
+	public void LoadMap(Hex[,] mapIn)
+	{
+		int x = mapIn.GetLength(0);
+		int y = mapIn.GetLength(1);
 
-    public void LoadMap(Hex[,] mapIn)
-    {
-        int x = mapIn.GetLength(0);
-        int y = mapIn.GetLength(1);
+		for (int column = 0, row = 0; row < y; row++)
+		{
+			for (column = 0; column < x; column++)
+			{
+				TileType tt = tileTypes[HexGrid[column, row].tileType];
+				GameObject hexagon = Instantiate(tt.tileVisualPrefab, TileCoordToWorldCoord(column, row), Quaternion.identity, transform);
+				hexagon.name = "Hex_" + column + "_" + row;
+				hexagon.isStatic = true;
+			}
+		}
+		StaticBatchingUtility.Combine(gameObject);
+	}
 
-        for (int column = 0, row = 0; row < y; row++)
-        {
-            for (column = 0; column < x; column++)
-            {
-                TileType tt = tileTypes[HexGrid[column, row].tileType];
-                GameObject hexagon = Instantiate(tt.tileVisualPrefab, TileCoordToWorldCoord(column, row), Quaternion.identity, transform);
-                hexagon.name = "Hex_" + column + "_" + row;
-                hexagon.isStatic = true;
-            }
-        }
-        StaticBatchingUtility.Combine(gameObject);
-    }
-
-    public void LoadMap()
-    {
-        LoadMap(HexGrid);
-    }
+	public void LoadMap()
+	{
+		LoadMap(HexGrid);
+	}
 }
